b28566ea09ca7b6098e1df17ea9bd819e092a65e
mani0524 <skmani2@gmail.com>
2020-04-26 17:32:35 +0530

    pom with nexus repo

---
+++ student-course-details/src/main/java/com/mss/learn/application/Microservice.java
@@ -0,0 +1,15 @@
+package com.mss.learn.application;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@SpringBootApplication
+public @interface Microservice {
+
+}
---
+++ student-course-details/src/main/java/com/mss/learn/news/News.java
@@ -0,0 +1,30 @@
+package com.mss.learn.news;
+
+
+public class News {
+private String title;
+private String section;
+private Results[] results;
+
+public String getTitle() {
+	return title;
+}
+public void setTitle(String title) {
+	this.title = title;
+}
+public String getSection() {
+	return section;
+}
+public void setSection(String section) {
+	this.section = section;
+}
+
+public Results[] getResults() {
+    return results;
+}
+
+public void setResults(Results[] results) {
+    this.results = results;
+}
+
+}
